98897d52e1476f9813e6dc3934f2776fb1b26ce9
amethysteDev971 <amethyste.design@gmail.com>
2024-12-12 09:30:09 -0400

    fixe css ListTile component

--- src/Listcontainer/ListTile/ListTile.jsx
+++ src/Listcontainer/ListTile/ListTile.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { useDispatch } from 'react-redux';
-import { openModal } from '../../redux/slice/ModalSlice';
+import { useDispatch } from 'react-redux'
+import { openModal } from '../../redux/slice/ModalSlice'
+import './ListTile.css'
 
 export default function ListTile({item, deleteAction}) {
    
