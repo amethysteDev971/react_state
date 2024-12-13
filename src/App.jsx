b2c993ee80b1fee64602b42100c85834407c443d
amethysteDev971 <amethyste.design@gmail.com>
2024-12-13 13:57:25 -0400

    feature add Nav et routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Listcontainer from './Listcontainer/Listcontainer';
 import './App.css';
 import Modal from './components/Modal/Modal';
 import { openModal } from './redux/slice/ModalSlice';
-import { Routes, Route } from "react-router";
+import { Routes, Route, NavLink, Link } from "react-router";
+import Acceuil from './pages/Acceuil/Acceuil';
+import Services from './pages/Service/Services';
 
 function App() {
   const dispatch = useDispatch();
@@ -14,6 +16,30 @@ function App() {
 
   return (
     <>
+    <div id="nav" className=''>
+      <div className='logo'>REACT</div>
+      <div className='container_nav flex-nav'>
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive ? "active" : ""
+        }
+      >Accueil</NavLink>
+      <NavLink
+        to="/services"
+        className={({ isActive }) =>
+          isActive ? "active" : ""
+        }
+      >Services</NavLink>
+        <NavLink
+        to="/taches"
+        className={({ isActive }) =>
+          isActive ? "active" : ""
+        }
+      >Tâches</NavLink>
+
+      </div>
+    </div>
       <div className='flex-inline'>
         <h1>Application To-do</h1>
         {/* <button onClick={handleOpenModal} className='btn-rouge'>
@@ -21,6 +47,8 @@ function App() {
         </button> */}
       </div>
       <Routes>
+          <Route path='/' element={<Acceuil/>}/>
+          <Route  path='/services' element={<Services/>}/>
           <Route path="/taches" element={<Listcontainer />} />
       </Routes>
       
---
+++ src/pages/Acceuil/Acceuil.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Acceuil() {
+  return (
+    <div>Page d'acceuil...</div>
+  )
+}
